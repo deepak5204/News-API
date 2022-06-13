36058c1349bc451ccdbd00490494592960fda06f
deepak <deepakkumar750206@gmail.com>
2022-06-13 15:35:39 +0530

    Fetch news news

--- index.js
+++ index.js
@@ -4,43 +4,48 @@ console.log("Welcome in news API project");
 
 
 //grab the news container
-let newsAccordian = document.getElementById('newsAccordian');
+let newsAccordian = document.getElementById('newsAccordion');
 
 //Initialize the news api parameter
 let apiKey = '15586a58b69d47b4956f2d71c0090981';
 
 //create an AJAX get request
 const xhr = new XMLHttpRequest();
-xhr.open('GET', `https://newsapi.org/v2/top-headlines?country=${apiKey}`, true)
+
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`, true)
 
 //What to do when response is ready
 xhr.onload = function(){
     if(this.status === 200){
-        console.log(this.responseText);
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+        console.log(articles);
+
+        let newsHtml = "";
+
+        articles.forEach(function(element){
+            // console.log(articles[element]);
+            let news = `<div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        ${element["title"]}
+                    </button>
+                </h2>
+                <div id="collapseOne" class=" collapse show" aria-labelledby="headingOne"
+                    data-bs-parent="#accordionExample">
+                    <div class="accordion-body">${element["description"]}</div>
+                </div>
+            </div>`
+            newsHtml += news;
+        });
+
+        newsAccordian.innerHTML = newsHtml;
     }
     else{
         console.log('Some error occured');
     }
 }
 
-xhr.send();
+xhr.send()
 
-let news = `<div class="accordion-item">
-                <h2 class="accordion-header" id="headingOne">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Accordion Item #1
-                    </button>
-                </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
-                    data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
-                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the
-                        collapse plugin adds the appropriate classes that we use to style each element. These classes
-                        control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                        modify any of this with custom CSS or overriding our default variables. It's also worth noting
-                        that just about any HTML can go within the <code>.accordion-body</code>, though the transition
-                        does limit overflow.
-                    </div>
-                </div>
-            </div>`
