d8a19ef0668a208283dd88bc5c359ca0abd35fd8
deepak <deepakkumar750206@gmail.com>
2022-06-13 18:16:21 +0530

    Now its ready for use

--- index.js
+++ index.js
@@ -24,24 +24,10 @@ xhr.onload = function () {
         let newsHtml = "";
 
         articles.forEach(function (element, index) {
-            // let news = `<div class="accordion-item">
-            //     <h2 class="accordion-header" id="heading${index}">
-            //         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse "
-            //             data-bs-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
-            //             ${element["title"]}
-            //         </button>
-            //     </h2>
-            //     <div id="collapse${index}" class="accordion-collapse collapse" aria-labelledby="heading${index}"
-            //         data-bs-parent="#newsAccordion">
-            //         <div class="accordion-body">${element["description"]}   .<a href = "${element['url']}" target = "_blank">Read more here</a>
-            //         </div>
-            //     </div>
-            // </div>`
-
             let news = ` <div class="accordion-item">
             <h2 class="accordion-header" id="newsAccordion">
               <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}" aria-expanded="false" aria-controls="collapse${index}">
-              ${element["title"]}
+             <b>Breaking News ${index + 1 } : &nbsp</b>${ element["title"]}
               </button>
             </h2>
             <div id="collapse${index}" class="accordion-collapse collapse" aria-labelledby="heading${index}" data-bs-parent="#newsAccordion">
